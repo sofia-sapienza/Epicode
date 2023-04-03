c26beacf1ca7a31210dc822626f1f8e674eddba2
sofia-sapienza <sofiasapienza99@libero.it>
2023-04-03 17:13:20 +0200

    adds index

---
+++ week2/day1/assets/JS/script.js
@@ -0,0 +1,6 @@
+window.alert('Ciao! Clicca ok per continuare');
+alert('Scherzavo, clicca ok un ultima volta');
+
+document.getElementById('title').innerHTML = 'Iniziamo la nostra pratica';
+
+document.write('<p class="paragrafi"><b>output scritto da Javascript</b></p><p class="paragrafi"><b>i quattro elementi scritti da Javascript</p>');
---
+++ week2/day1/assets/JS/second.js
@@ -0,0 +1,12 @@
+var primoElemento = 'aria';
+
+var SecondoElemento = 'acqua';
+
+var terzoelemento = 'terra';
+
+var quarto_Elemento = 'fuoco';
+
+console.log(primoElemento);
+console.log(SecondoElemento);
+console.log(terzoelemento);
+console.log(quarto_Elemento);
