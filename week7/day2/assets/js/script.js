ff62f4f1f85493c9994d547ba37ce1f0f10f10e9
sofia-sapienza <sofiasapienza99@libero.it>
2023-05-09 15:43:24 +0200

    adds index

---
+++ week7/day2/assets/js/script.js
@@ -0,0 +1,11 @@
+// questo form salverà in localStorage il nome e cognome dell'utente
+// alla pressione del tasto "SALVA", dovremo inserire un nuovo nome alla lista già presente
+ let namesList = [];
+
+ // all'avvio della pagina sarà necessario ripartire dalla lista di nomi già salvata, se la troveremo in localStorage
+
+ // prendo i riferimenti agli input field
+ const name = document.getElementById('name');
+ const surname = document.getElementById('surname');
+ 
+
