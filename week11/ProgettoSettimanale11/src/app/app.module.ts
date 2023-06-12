1a25dff46b0fb2f36d5dc518e9591aafa78aaf60
Sofia Sapienza <sofiasapienza99@libero.it>
2023-06-12 12:15:29 +0200

    adds index

--- week11/ProgettoSettimanale11/src/app/app.module.ts
+++ week11/ProgettoSettimanale11/src/app/app.module.ts
@@ -2,20 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { MoviesComponent } from './components/movies/movies.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './auth/login/login/login.component';
-import { RegisterComponent } from './auth/register/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProfileComponent,
-    MoviesComponent,
-    NavbarComponent,
-    LoginComponent,
-    RegisterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
