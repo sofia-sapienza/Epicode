518d551494c01d51c5cdc73c1b5808e605a9fc0b
Sofia Sapienza <sofiasapienza99@libero.it>
2023-06-12 18:11:19 +0200

    adds index

--- week11/ProgettoSettimanale11/src/app/app.module.ts
+++ week11/ProgettoSettimanale11/src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//Per gestire il routing importo {Route, RouterModule} from '@angular/router';
+import {Route, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
+const routes: Route[] = [
+ {
+  path: '',
+  component: HomeComponent
+ },
+ {
+  path: 'movies',
+  component: MoviesComponent
+ },
+ {
+  path: 'profile',
+  component: ProfileComponent
+ }
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
     MoviesComponent,
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
