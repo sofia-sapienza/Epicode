b2c88771fd2f1ec3896a85e5a07b67268fff875e
Sofia Sapienza <sofiasapienza99@libero.it>
2023-06-13 18:15:05 +0200

    adds index

--- week11/ProgettoSettimanale11/src/app/app.module.ts
+++ week11/ProgettoSettimanale11/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 //Per gestire il routing importo {Route, RouterModule} from '@angular/router';
-import {Route, RouterModule} from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -11,24 +11,44 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { Error404Component } from './components/error404/error404.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { DetailsComponent } from './components/details/details.component';
 
 const routes: Route[] = [
- {
-  path: '',
-  component: HomeComponent
- },
- {
-  path: 'movies',
-  component: MoviesComponent
- },
- {
-  path: 'profile',
-  component: ProfileComponent
- },
- {
-  path: '**',
-  component: Error404Component
- }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    children: [
+      {
+        path: 'details',
+        component: DetailsComponent,
+      },
+      {
+        path: 'favorites',
+        component: FavoritesComponent,
+      }
+    ]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: '**',
+    component: Error404Component,
+  },
 ];
 @NgModule({
   declarations: [
@@ -39,13 +59,12 @@ const routes: Route[] = [
     ProfileComponent,
     LoginComponent,
     RegisterComponent,
-    Error404Component
+    Error404Component,
+    FavoritesComponent,
+    DetailsComponent,
   ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes)
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
